870f84e2aae7a92a0b8dbe26390a0b3b1a608fa4
LFMP <luizfellipe42@hotmail.com>
2020-01-15 12:37:29 -0300

    geracao de solucao e calculo de fitness

---
+++ main.py
@@ -0,0 +1,110 @@
+import math
+import heapq
+import random
+
+
+class no:
+    x: int
+    y: int
+    capacidade: int
+    ocupado: int
+    peso: int
+    ligacoes: []
+    key: int
+
+    def __init__(self, x, y, capacidade, ocupado, peso, ligacoes, key):
+        self.x = x
+        self.y = y
+        self.capacidade = capacidade
+        self.peso = peso
+        self.ligacoes = ligacoes
+        self.key = key
+        self.ocupado = ocupado
+
+    def __gt__(self, other):
+        if not isinstance(other, no):
+            # don't attempt to compare against unrelated types
+            return NotImplemented
+        return self.key > other.key
+
+    def __lt__(self, other):
+        if not isinstance(other, no):
+            # don't attempt to compare against unrelated types
+            return NotImplemented
+        return self.key < other.key
+
+    def toString(self):
+        print(
+            "{ x : %d, y : %d, capacidade : %d, ocupado : %d, peso : %d, indice : %d }"
+            % (self.x, self.y, self.capacidade, self.ocupado, self.peso, self.key)
+        )
+
+
+def calculaDistancia(x1, y1, x2, y2):
+    return math.sqrt(math.pow((x2 - x1), 2) + math.pow((y2 - y1), 2))
+
+
+def montaConjuto(nos):
+    lido = input().split(" ")
+    lido = [num for num in lido if num]
+    qtdvertice, qtdmedianas = int(lido[0]), int(lido[1])
+    for i in range(qtdvertice):
+        lido = input().split(" ")
+        lido = [num for num in lido if num]
+        nos.update(
+            {
+                str(i): no(
+                    int(lido[0]),
+                    int(lido[1]),
+                    int(lido[2]),
+                    int(lido[3]),
+                    int(lido[3]),
+                    [],
+                    i,
+                )
+            }
+        )
+    return qtdmedianas
+
+
+def sortMedianas(nos, medianas, qtdmedianas):
+    selecionados = []
+    selecionados.append(random.randrange(0, len(nos)))
+    for i in range(qtdmedianas):
+        r = random.randrange(0, len(nos))
+        while r in selecionados:
+            r = random.randrange(0, len(nos))
+        selecionados.append(r)
+        element = nos[str(r)]
+        medianas.append(element)
+        nos.pop(str(element.key))
+
+
+def montaIndividuo(nos, medianas):
+    fitness = 0
+    for i in nos:
+        menorDistancia = []
+        for (indice, j) in enumerate(medianas):
+            heapq.heappush(
+                menorDistancia, (calculaDistancia(nos[i].x, nos[i].y, j.x, j.y), j)
+            )
+        while menorDistancia != []:
+            aux = heapq.heappop(menorDistancia)
+            if aux[1].ocupado + nos[i].peso < aux[1].capacidade:
+                fitness += calculaDistancia(nos[i].x, nos[i].y, aux[1].x, aux[1].y)
+                aux[1].ligacoes.append(nos[i].key)
+                aux[1].ocupado += nos[i].peso
+                menorDistancia = []
+            else:
+                aux = heapq.heappop(menorDistancia)
+    return fitness
+
+
+nos = {}
+medianas = []
+individuos = []
+populacao = {}
+qtdmedianas = montaConjuto(nos)
+sortMedianas(nos, medianas, qtdmedianas)
+fitness = montaIndividuo(nos, medianas)
+print(fitness)
